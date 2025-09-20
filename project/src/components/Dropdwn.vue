8cd7df4763380d9d3090b0f75047f63150dcae2c
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-09-20 14:13:24 -0400

    made dropdown its own component

---
+++ project/src/components/Dropdwn.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let selected = ref('');
+
+defineExpose(selected)
+
+</script>
+
+<template>
+    <div>
+        <form class="dropCont">
+    <div >
+      <div>
+        <label for="contacts">Choose a contact</label>
+      </div>
+      
+      <select name="contacts" id="contacts"
+      v-model="selected">
+        <option disabled value="">Choose User</option>
+        <option>One</option>
+        <option>Two</option>
+        <option>Three</option>
+      </select>
+    </div>
+    </form>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
