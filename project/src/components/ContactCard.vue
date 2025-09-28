7a67c9674910084f88235f466b9556a371e23261
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-09-28 11:31:49 -0400

    added contact info to card

--- project/src/components/ContactCard.vue
+++ project/src/components/ContactCard.vue
@@ -10,16 +10,24 @@ const props = defineProps({
 </script>
 
 <template>
-    <div>
+    <div id="contactCardCont">
       <h3>Contact Info</h3>
       <div>
-        <h4>{{ selectedUser.name }}</h4>
-        <p>{{ selectedUser.email }}</p>
+        <h4>Name: {{ selectedUser.name }}</h4>
+        <p>Username: {{ selectedUser.username }}</p>
+        <p>Email: {{ selectedUser.email }}</p>
+        <p>Phone: {{ selectedUser.phone }}</p>
+        <p>Website <a href="{{ selectedUser.website }}" _target="blank">{{ selectedUser.website }}</a></p>
       </div>  
       
     </div>
 </template>
 
 <style scoped>
+
+#contactCardCont {
+  background-color: #978e8e;
+  
+}
 
 </style>
